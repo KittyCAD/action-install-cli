a1d75039bdab79efa671227639dfb3ec6e138acf
Kurt Hutten IrevDev <k.hutten@protonmail.ch>
2022-06-21 11:54:27 +1000

    update cli to v0.1.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 # Export the sha256sum for verification.
-export KITTYCAD_CLI_SHA256="ecb8310fdb91ea44e545a19d88ebbc3c391835bc00978679030746bcb85c5031"
+export KITTYCAD_CLI_SHA256="5bd98de5e17086a6ee3655842df3e8126c74c1acde416c7ae98ca9b5c2581fdb"
 
 
 # Download and check the sha256sum.
-curl -fSL "https://dl.kittycad.io/releases/cli/v0.0.9/cli-linux-amd64" -o "/usr/local/bin/kittycad" \
+curl -fSL "https://dl.kittycad.io/releases/cli/v0.1.1/kittycad-x86_64-pc-windows-gnu" -o "/usr/local/bin/kittycad" \
 	&& echo "${KITTYCAD_CLI_SHA256}  /usr/local/bin/kittycad" | sha256sum -c - \
 	&& chmod a+x "/usr/local/bin/kittycad"
 
