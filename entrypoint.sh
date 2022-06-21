6d65edf622df4d53ede51da5000426e5c6092d02
Kurt Hutten IrevDev <k.hutten@protonmail.ch>
2022-06-21 12:10:50 +1000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 # Export the sha256sum for verification.
-export KITTYCAD_CLI_SHA256="5bd98de5e17086a6ee3655842df3e8126c74c1acde416c7ae98ca9b5c2581fdb"
+export KITTYCAD_CLI_SHA256="73df1986bb0d1c97f72a9bb79298a464872fbf1e4c097624b911d001e7a73da1"
 
 
 # Download and check the sha256sum.
-curl -fSL "https://dl.kittycad.io/releases/cli/v0.1.1/kittycad-x86_64-pc-windows-gnu" -o "/usr/local/bin/kittycad" \
+curl -fSL "https://dl.kittycad.io/releases/cli/v0.1.1/kittycad-x86_64-unknown-linux-musl" -o "/usr/local/bin/kittycad" \
 	&& echo "${KITTYCAD_CLI_SHA256}  /usr/local/bin/kittycad" | sha256sum -c - \
 	&& chmod a+x "/usr/local/bin/kittycad"
 
