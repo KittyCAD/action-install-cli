e951201d3e9e78a338b701adcb1b55a062f5bd34
49fl <ircsurfer33@gmail.com>
2024-03-13 00:24:46 -0400

    Use latest version of cli, and hashes from GitHub (#21)

--- entrypoint-win.sh
+++ entrypoint-win.sh
@@ -1,19 +1,25 @@
 # Export the sha256sum for verification.
-export KITTYCAD_CLI_SHA256="ac4b9ad2863eea0c22ef43a958d23c2c55755009d6f41e81cbe1e7c0334ed3a9"
-
 mkdir "C:\Program Files\Zoo"
 
+# Add path for this shell to test cli
+export PATH=$PATH:"/C/Program Files/Zoo"
+
+# Add to Github path for later shells
+echo "C:\Program Files\Zoo" >> $GITHUB_PATH
+
+# The latest release
+RELEASE=$(curl "https://api.github.com/repos/KittyCAD/cli/tags" | jq -r ".[0].name")
+
+# Get the SHA256 hash of the latest release
+KITTYCAD_CLI_SHA256=$(curl -L "https://github.com/KittyCAD/cli/releases/download/$RELEASE/zoo-x86_64-pc-windows-gnu.sha256" | cut -d ' ' -f 1)
+
 # Download and check the sha256sum.
-curl -fSL "https://dl.zoo.dev/releases/cli/v0.2.21/zoo-x86_64-pc-windows-gnu" -o "C:\Program Files\Zoo\zoo.exe" \
+curl -fSL "https://dl.zoo.dev/releases/cli/$RELEASE/zoo-x86_64-pc-windows-gnu" -o "C:\Program Files\Zoo\zoo.exe" \
 	&& echo "${KITTYCAD_CLI_SHA256}  C:\Program Files\Zoo\zoo.exe" | sha256sum -c - \
 	&& chmod a+x "C:\Program Files\Zoo\zoo.exe"
 
 echo "zoo cli installed!"
 
-# Add path for this shell to test cli
-export PATH=$PATH:"/C/Program Files/Zoo"
 # Run it!
 zoo -h
 
-# Add to Github path for later shells
-echo "C:\Program Files\Zoo" >> $GITHUB_PATH
