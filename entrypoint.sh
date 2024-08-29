02523d2a1edde862e6769edf1853bcc84b041018
49lf <ircsurfer33@gmail.com>
2024-08-29 16:58:23 -0400

    Add early exits and use releases instead of tags

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,24 @@
+#!/bin/sh
+
+# This does not cover captured subshells.
+set -e
+
 # The latest release
-RELEASE=$(curl "https://api.github.com/repos/KittyCAD/cli/tags" | jq -r ".[0].name")
+RELEASE_JSON=$(curl "https://api.github.com/repos/KittyCAD/cli/releases")
+if [ ! $? -eq 0 ]; then exit $?; fi
+
+RELEASE=$(echo "$RELEASE_JSON" | jq -r ".[0].name")
+if [ ! $? -eq 0 ]; then exit $?; fi
 
 # Get the SHA256 hash of the latest release
-KITTYCAD_CLI_SHA256=$(curl -L "https://github.com/KittyCAD/cli/releases/download/$RELEASE/zoo-x86_64-unknown-linux-musl.sha256" | cut -d ' ' -f 1)
+KITTYCAD_CLI_SHA256=$(curl -L -f "https://github.com/KittyCAD/cli/releases/download/$RELEASE/zoo-x86_64-unknown-linux-musl.sha256" | cut -d ' ' -f 1)
+if [ ! $? -eq 0 ]; then exit $?; fi
 
 # Download and check the sha256sum.
-curl -fSL "https://dl.zoo.dev/releases/cli/$RELEASE/zoo-x86_64-unknown-linux-musl" -o "/usr/local/bin/zoo" \
+curl -fSL "https://github.com/KittyCAD/cli/releases/download/$RELEASE/zoo-x86_64-unknown-linux-musl" -o "/usr/local/bin/zoo" \
 	&& echo "${KITTYCAD_CLI_SHA256} /usr/local/bin/zoo" | sha256sum -c - \
 	&& chmod a+x "/usr/local/bin/zoo"
+if [ ! $? -eq 0 ]; then exit $?; fi
 
 echo "zoo cli installed!"
 
